4facd6a4bc99d611e8b1c42630044961e1b33e2e
Michal Debicki (ext) <michal.debicki@atos.net>
2026-02-27 10:50:57 +0100

    Trigger CI run

--- tests/test_login_functional.py
+++ tests/test_login_functional.py
@@ -6,7 +6,7 @@ from playwright.sync_api import expect
 def test_valid_login(page):
     login_page = LoginPage(page)
     login_page.open()
-    login_page.login("tomsmith", "SuperSecretPassword!")
+    login_page.login("tomsmith", "SuperSecretPassword!") #comment
     dashboard = DashboardPage(page)
     expect(dashboard.welcome_message).to_contain_text("You logged into a secure area")
     dashboard.logout()
